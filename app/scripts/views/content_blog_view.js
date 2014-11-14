ce6c33402c3720a07ebbef4fc0164e8030490518
Maggie Martin <maggierozay@gmail.com>
2014-11-14 14:52:23 -0500

    comments appearing but theres a delay

--- app/scripts/views/content_blog_view.js
+++ app/scripts/views/content_blog_view.js
@@ -4,7 +4,7 @@ App.Views.FullContentView = Parse.View.extend({
  tagNamme: 'ul',
  className: 'fullPost',
  events:{
-   //'submit #newComment' : 'addComment'
+   'submit #addComment' : 'addComment'
  },
  template : _.template($('#singlePostTemp').html()),
 
@@ -16,25 +16,47 @@ App.Views.FullContentView = Parse.View.extend({
     $('#blogList').html(this.$el);
   },
   render: function(){
-    this.$el.html(this.template(this.post.toJSON()));
+  this.$el.empty();  this.$el.html(this.template(this.post.toJSON()));
+
+  var commentTemplate = _.template($('#commentTemp').html());
+      var comments_query = new Parse.Query(App.Models.Comment);
+      comments_query.equalTo('parent', this.options.post);
+
+        this.$el.append('<div class="comment-section"><h2>Comments</h2><ul class="comments"></ul></div>');
+
+   comments_query.find({
+        success: function (results) {
+
+          _.each(results, function(comment) {
+            $('ul.comments').append(commentTemplate(comment.toJSON()));
+          })
+
+       }
+     })
+
   },
-  // addComment: function(e){
-  //   e.preventDefault();
-  //
-  //   var comment = new App.Models.Comment({
-  //
-  //     commentText: $('#commentBox').val(),
-  //     parent: this.options.post
-  //
-  //   });
-  //
-  //   comment.save(null, {
-  //     success: function () {
-  //       console.log('Comment has been added');
-  //       App.router.navigate('globalPosts', {trigger: true});
-  //     }
-  //   });
-  // }
+
+
+
+  addComment: function (e) {
+    e.preventDefault();
+
+    var comment = new App.Models.Comment({
+
+      commentText: $('#commentText').val(),
+      parent: this.options.post
+
+    });
+
+    comment.save(null, {
+      success: function () {
+        console.log('Comment has been added');
+        App.router.navigate('', {trigger: true});
+      }
+    });
+
+  }
+
 
 });
 }());
