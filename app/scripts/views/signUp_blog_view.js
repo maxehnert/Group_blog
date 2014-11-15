736fd0f63ffbe55557083bc8ac8ddf1f6f5281e8
Maggie Martin <maggierozay@gmail.com>
2014-11-15 02:12:07 -0500

    styled global and myposts, have to use new users to see new posts though

--- app/scripts/views/signUp_blog_view.js
+++ app/scripts/views/signUp_blog_view.js
@@ -18,10 +18,8 @@
     addUser: function(e){
       e.preventDefault();
       var user = new Parse.User({
-
       username: $('#signUpUser').val(),
       password: $('#signUpPassword').val()
-    
 
       });
       user.signUp(null, {
