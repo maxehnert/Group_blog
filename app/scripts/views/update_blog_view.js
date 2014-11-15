662837a7abc37ad15c3d0bbb8a082893e1ad0088
Maggie Martin <maggierozay@gmail.com>
2014-11-15 18:36:39 -0500

    done styling edit post template, however getting errors with drafts now

--- app/scripts/views/update_blog_view.js
+++ app/scripts/views/update_blog_view.js
@@ -39,7 +39,8 @@
       this.options.post.set({
         title: $('#updateTitle').val(),
         content: $('#updateContent').val(),
-        category: $('#updateCategory').val()
+        category: $('#updateCategory').val(),
+        image: $('#UpdatepostImage').val()
       });
 
       if (draft != undefined) {
